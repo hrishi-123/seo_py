8230af63146bc9548f58681ac2842df05791ce38
hrishi-123 <99014238+hrishi-123@users.noreply.github.com>
2022-03-18 11:47:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,56 +14,7 @@ import time
 # This is needed to convert API 
 # responses into JSON objects
 import json
-'''
-# Godaddy developer key and secret
-api_key = "your api_key"
-secret_key = "your secret_key"
 
-# API key and secret are sent in the header
-headers = {"Authorization" : "sso-key {}:{}".format(api_key, secret_key)}
-
-# Domain availability and appraisal end points
-url = "https://api.ote-godaddy.com/v1/domains/available?checkType=FULL"
-appraisal = "https://api.ote-godaddy.com/v1/appraisal/{}"
-
-# If a domain name is available 
-# decide whether to appraise or not
-do_appraise = True
-
-# Number of domains to check in each call. 
-# For example, we can not check more than 500 
-# domain names in one call so we need to split 
-# the list of domain names into chunks
-
-# Filter domain names by length
-max_length = 30
-
-all_domains = pd.read_excel("/content/drive/MyDrive/Data Science/backlinks.xlsx", engine='openpyxl')
-
-df = pd.DataFrame(columns= ["Domains"])
-#for domains in domain_chunks:
-   # Get availability information by calling availability API
-for x in range(0, len(all_domains)):
-
-        # Define request parameter
-      dom = all_domains.iloc[x]["domains"]
-      availability_res = requests.get('https://api.ote-godaddy.com/v1/domains/available?domain={}&checkType=FAST'.format(dom), headers=headers);
-      time.sleep(1)
-        # Get only available domains with price range
-      result = json.loads(availability_res.text)
-      #if (availability_res.text.find('"available":true') != -1):
-      if "available" in availability_res.text:
-        if result["available"]==True:
-          if result["definitive"]==True: 
-            #price = float(result["price"])/1000000 
-            #print("{:30} : {:10} {:10}".format(result["domain"], price, result["currency"]))
-            df.loc[x, 'Domains'] = result["domain"]
-            df.to_excel('/content/drive/MyDrive/work/domains.xlsx', sheet_name= 'sheet1')
-            print(result["domain"])
-            print(x+1)
-
-###############################################
-'''
 c30, c31, c32, c33, c34 = st.beta_columns(5)
 
 with c30:
